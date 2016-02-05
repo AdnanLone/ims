cd013b4d820118bbe20bfbe92f8e21c8a3ff7ae9
Adnan Lone <adnanlone13@gmail.com>
2016-02-05 17:22:13 -0500

    Basic Features of IMS

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,90 @@
-/**
- * Created by adnanlone on 2/3/16.
- */
+window.onload = function () {
+
+    var queryForm = document.getElementById('queryProducts');
+    var orderForm = document.getElementById('orderProducts');
+    var restockForm = document.getElementById('restockProducts');
+    var ws = new WebSocket('ws://localhost:4080');
+
+    ws.onopen = function () {
+        console.log("connected to server");
+
+    ws.onmessage = function (msg) {
+        var serverResponse = JSON.parse(msg.data);
+        console.log(serverResponse);
+        var ul ="";
+        var divid;
+
+    };//end on message
+
+        //update window function
+        function updateWindow(id,ul){
+            document.getElementById(id).innerHTML = ul;
+        }
+
+    };//end on open
+
+    queryForm.onsubmit = function (e) {
+        e.preventDefault();
+        var queryProduct = document.getElementById('pquery');
+        var queryProductName = queryProduct.value;
+        console.log("Querying for " + queryProductName);
+        var product_name = {
+            type: "query",
+            product_name: queryProductName
+        };
+        var query_product = JSON.stringify(product_name);
+        ws.send(query_product);
+    };
+
+    orderForm.onsubmit = function (e) {
+        e.preventDefault();
+        var orderProductName = document.getElementById('pOName').value;
+        var orderProductQuantity = document.getElementById('pquantity').value;
+        var product_id;
+        if (orderProductName === "iphone"){
+            product_id = "prod1"
+        }else if (orderProductName === "MacBook"){
+            product_id = "prod2"
+        }else{
+            console.log("Please enter either iphone OR MacBook");
+        }
+        console.log("Order For " + orderProductName);
+
+        var orderDetails ={
+            type: "order",
+            product_id: product_id,
+            order_quantity: orderProductQuantity
+        };
+        var order = JSON.stringify(orderDetails);
+        ws.send(order);
+    };
+
+    restockForm.onsubmit = function (e) {
+        e.preventDefault();
+
+        var restockProductName = document.getElementById('pRName').value;
+        var restockProductQuantity = document.getElementById('prestock').value;
+        var restockProductId;
+        if (restockProductName === "iphone"){
+            restockProductId = "prod1"
+        }else if (restockProductName === "MacBook"){
+            restockProductId = "prod2"
+        }else{
+            console.log("Please enter either iphone OR MacBook");
+        }
+        console.log("Restocking For " + restockProductName);
+
+        var restockDetails ={
+            type:"restock",
+            product_id: restockProductId,
+            restock_quantity: restockProductQuantity
+        };
+        var restock = JSON.stringify(restockDetails);
+        ws.send(restock);
+    };
+
+    ws.onclose = function () {
+        console.log('disconnected');
+    };
+};//end window.onload
+
--- server.js
+++ server.js
@@ -4,7 +4,10 @@ var server = require('http').createServer()
     , wss = new WebSocketServer({ server: server })
     , express = require('express')
     , app = express()
-    , PORT = process.env.PORT ||4080;
+    , PORT = process.env.PORT || 4080;
+
+var dblite = require('dblite'),
+    db = dblite('/Users/Adnan/Desktop/ims/products.db');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -15,15 +18,96 @@ app.use(function (req, res) {
 
 wss.on('connection', function connection(ws) {
     var location = url.parse(ws.upgradeReq.url, true);
-    // you might use location.query.access_token to authenticate or share sessions
-    // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
 
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
-    });
+        var receivedMessage = JSON.parse(message);
 
-    ws.send('something');
-});
+        switch (receivedMessage.type){
+            case "query":
+                dbQuery(receivedMessage.product_name);
+                break;
+
+            case "order":
+                orderProducts(receivedMessage.product_id,receivedMessage.order_quantity);
+                break;
+
+            case "restock":
+                restockProducts(receivedMessage.product_id,receivedMessage.restock_quantity);
+                break;
+        }//end switch case
+    });//end onmessage
+
+    //order function
+    function orderProducts(productId,orderQuantity) {
+        //check if the product is available
+        db.query("select product_quantity from products where product_id=?", [productId],
+            {
+                product_quantity: Number
+            },
+            function (rows) {
+                var result = rows[0];
+                var availableQuantity = parseInt(result.product_quantity);
+                //check if stock exists
+                if (availableQuantity >= parseInt(orderQuantity)) {
+                    db.query("update products SET product_quantity= product_quantity - ? where product_id=?", [orderQuantity, productId]);
+                    var orderSucess = "Your Order has been processed. thank you";
+                    console.log(orderSucess);
+                    var successOrderResponse = {
+                        response_type: "orderProducts",
+                        response_status: orderSucess
+                    };
+                    ws.send(JSON.stringify(successOrderResponse));
+                } else {
+                    var outOfStock = "Sorry Order Cannot Be Processed / Out of Stock";
+                    console.log(outOfStock);
+                    var failedOrderResponse = {
+                        response_type: "orderProducts",
+                        response_status: outOfStock
+                    };
+                    ws.send(JSON.stringify(failedOrderResponse));
+                }
+            });
+    }//end order
+
+    ////restock function
+    function restockProducts(productId,restockQuantity){
+        db.query("update products SET product_quantity = product_quantity + ? where product_id=?",[restockQuantity,productId]);
+        var restocked = "Product has been restocked by " + restockQuantity ;
+        console.log(restocked);
+        var restockResponse = {
+            response_type: "restockProducts",
+            response_status: restocked
+        };
+        ws.send(JSON.stringify(restockResponse));
+    }//end restock
+
+    //db query
+    function dbQuery(query_product){
+        db.query("select * from products where product_name=?",[query_product] ,
+        {
+            product_id: String,
+            product_name: String,
+            product_location: String,
+            product_quantity: Number
+        },
+        function (rows) {
+            var result = rows[0];
+            var product_name = result.product_name;
+            var product_location = result.product_location;
+            var product_quantity = result.product_quantity;
+
+            var queryMessage = {
+                response_type: "queryProducts",
+                product_name: product_name,
+                product_location: product_location,
+                product_quantity: product_quantity
+            };
+            var returnMessage = JSON.stringify(queryMessage);
+            ws.send(returnMessage);
+        });//end db query
+}//end dbQuery function
+});//end on connection
 
 server.on('request', app);
 server.listen(PORT, function () { console.log('Listening on ' + server.address().port) });
